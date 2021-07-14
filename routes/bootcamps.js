bc3615831eaa38878cf6cbb80ebeac10f8756310
Vishwash Bajpai <vishwash.bajpai87@gmail.com>
2021-07-14 15:08:55 +0530

    adding protect middleware

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -7,12 +7,13 @@ const {
   createBootcamp,
   getBootcampsInRadius
 } = require('../controllers/bootcamps');
+const { protect } = require('../middleware/auth');
 
 const router = express.Router();
 
 router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 
-router.route('/').get(getBootcamps).post(createBootcamp);
+router.route('/').get(getBootcamps).post(protect, createBootcamp);
 
 router.route('/:id').get(getBootcamp).put(updateBootcamp).delete(deleteBootcamp);
 
